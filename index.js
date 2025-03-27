23dfeb32927e2387af616c42749523412f2ba861
zaki-s <zakijames024@gmail.com>
2025-03-27 22:06:39 +0300

    did tasks

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+// 1. Declare a global variable called customerName
+var customerName = "bob";
+
+// 2. Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Function to set bestCustomer (declaring a global variable inside a function)
+function setBestCustomer() {
+  bestCustomer = "not bob"; // Implicitly global (bad practice, but for learning purposes)
+}
+
+// 4. Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+// 5. Declare a constant leastFavoriteCustomer
+const leastFavoriteCustomer = "someone annoying";
+
+// 6. Function to attempt changing leastFavoriteCustomer (will cause an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "definitely not bob"; // This will throw an error!
+}
